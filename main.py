0f8caba460666cabdf22d37784834d3e781f1a6d
Tucker <tucker@neurable.com>
2025-11-22 09:47:22 -0500

    Change EEG source to HUB_IP with SSL context
    Updated connection details to use HUB_IP and added SSL context for testing.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import asyncio
 import json
+import ssl
 
 import websockets
 
-EEG_SOURCE_URL = "wss://fill-me-in-after-signing-nda"
+HUB_IP = (
+    "stream1.mindfulmakers.xyz"  # OR stream2.mindfulmakers.xyz
+)
 
 
 async def main():
-    print(f"Connecting to {EEG_SOURCE_URL}")
-    async with websockets.connect(EEG_SOURCE_URL) as ws:
+    print(f"Connecting to {HUB_IP}")
+    # Disable SSL certificate verification (for testing only)
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    
+    async with websockets.connect(f"wss://{HUB_IP}", ssl=ssl_context) as ws:
         async for msg in ws:
             eeg = json.loads(msg)
+            # process eeg
             print(eeg)
 
 
